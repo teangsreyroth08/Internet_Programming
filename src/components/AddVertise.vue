6c3d035ce03e86400fbd31d7bbf24c0705d4a6ca
teang sreyroth <teangsreyroth80@gmail.com>
2023-11-16 20:33:25 +0700

    My first commit

---
+++ src/components/AddVertise.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="container" :style="{backgroundColor: bgColor}">
+        <div class="container1">
+            <div class="heading">
+                {{ heanding }}
+            </div>
+            <div class="_btn">
+                <MyButton  :text="textBtn" :Bgcolor="colorBtn"/>
+            </div>
+        </div>
+        <div class="image">
+            <img :src=Img alt="">
+        </div>
+
+    </div>
+</template>
+
+<script>
+import MyButton from './MyButton.vue';
+
+export default{
+    name: 'AddVertise',
+    props: {
+        heanding: {
+            type: String,
+            default: 'New Collection',
+        },
+        bgColor: {
+            type: String,
+            default: '#3BB77E',
+        },
+        textBtn: {
+            type: String,
+            default: 'Show Now',
+        },
+        colorBtn: {
+            type: String,
+            default: '#3d3d3d',
+        },
+        Img: {
+            type: String,
+            default: 'https://picsum.photos/200/300', 
+        },
+    },
+    components: {
+        MyButton,
+    },
+}
+
+</script>
+
+<style scoped>
+
+.container {
+    width: 455px;
+    height: 300px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    border: 1px ;
+}
+.container1 {
+    width: 200px;
+    height: 100px;
+    margin-left: 50px;
+    margin-top: 70px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+}
+.heading {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;;
+    font-size: 25px;
+    font-weight: 700;
+    line-height: 30px;
+    color: #000;
+}
+._btn {
+    margin-top: 50px;
+}
+.image {
+    width: 260px;
+    height: 250px;
+    margin-top: 50px;
+    justify-content: center;
+    align-items: center;
+}
+
+img {
+    width: 100%;
+    height: 100%;
+}
+
+</style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,62 @@
+<template>
+  <button :style="{backgroundColor: Bgcolor}" class="button">
+    <span class="text">
+      {{ text }}
+    </span>
+     
+    <font-awesome-icon :icon ="['fas', 'arrow-right-long']" class="arrow" />
+  </button>
+</template>
+
+<script>
+  import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+  import { fas } from "@fortawesome/free-solid-svg-icons";
+  import { library } from "@fortawesome/fontawesome-svg-core";
+
+  library.add(fas);
+
+export default {
+  name: "MyButton.vue",
+  props: ["text", "Bgcolor"],
+  
+  components: {
+    FontAwesomeIcon,
+  },
+}
+</script>
+
+<style scoped>
+button {
+  border: none;
+  border-radius: 3px;
+  width: 110px;
+  height: 30px;
+  top: 180px;
+  left: 48px;
+  gap: 10px;
+  padding: 8px, 15px;
+  cursor: pointer;
+}
+button:hover {
+  opacity: 0.8;
+  box-shadow: 0px 4px 4px rgba(141, 238, 167, 0.565);
+}
+.text {
+  margin-right: 10px;
+  font-family: Quicksand;
+  font-weight: 700px;
+  font-size: 12px;
+  line-height: 15px;
+  width: 50px;
+  height: 15px;
+  text-align: center;
+  color: #ffffff;
+}
+.arrow {
+  font-size: 12px;
+  line-height: 15px;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+}
+</style>
